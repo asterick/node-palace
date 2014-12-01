cdcd983fd9b3176cb9a7c8ae687e940377409d7b
Bryon Vandiver <bryon@tilt.com>
2014-11-30 23:02:41 -0800

    Fixed typeo

--- tools/proxy.js
+++ tools/proxy.js
@@ -76,11 +76,9 @@ PacketStream.prototype.drain = function () {
 
 		return true;
 	} catch (e) {
-		/*
 		console.log("OUT OF SYNC ERROR:", this._target);
 		console.log(e.message);
 		dumpHex(this._buffer);
-		*/
 
 		// Incomplete message (or bad definition)
 		return false;
